1a10d472e2f22c1f1c054ab59a1f6d66c1a60e2b
kantoo21 <onnieainnah@gmail.com>
2024-03-07 14:30:49 +0400

    Fonction qui permet de changer

--- dataTask.py
+++ dataTask.py
@@ -2,4 +2,6 @@ def sauvegarder_dictionnaire_json(dictionnaire, TP.json):
    with open(TP.json,"w") as fichier_json:
 	json.dump(dictionnaire,fichier json)
 
-
+def changer_fichier_json(TP.json):
+   with open(TP.json,"r") as fichier:
+        return json.load(fichier)
