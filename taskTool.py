d8b1873d14882d5624fe19699744ccedb2cb0cc0
rdesii <desirenaroi@gmail.com>
2024-03-07 13:46:11 +0400

    taskTool.py

---
+++ taskTool.py
@@ -0,0 +1,4 @@
+
+tasks = {"task1":"Setup de projet git","task2":"modification de tache","task3":"Stocker les taches dans l'ordinateur","task4":"Mise en service"}
+def ajoute_tache(task_dict,nouvelle_tache, description):
+	task_dict[nouvelle_tache]= description
